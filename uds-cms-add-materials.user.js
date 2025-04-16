b2a73bb1bf1a05386446dd5db0dcc4c7702e5c9d
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-04-16 20:45:23 +0200

    Add UdS materials userscript
    The script adds in the navbar of a course a link to the materials of the
    course.

---
+++ uds-cms-add-materials.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        CMS Materials
+// @namespace   Violentmonkey Scripts
+// @match       https://cms.sic.saarland/*
+// @exclude-match https://cms.sic.saarland/system/*
+// @grant       none
+// @version     0.1.0
+// @author      Alexander Ikonomou
+// @description 14/04/2025, 17:27:34
+// ==/UserScript==
+
+const navbar = document.querySelector("#navbar-main");
+
+const lecture = window.location.pathname.replace(/^\/(.*?)(\/.*)/, "$1");
+
+if (navbar == null) {
+  return;
+}
+
+const item = document.createElement("li");
+item.classList.add("nav-item");
+
+const link = document.createElement("a");
+link.href = `/${lecture}/materials`;
+link.classList.add("nav-link");
+
+const text = document.createTextNode("Materials");
+
+link.appendChild(text);
+item.appendChild(link);
+navbar.appendChild(item);
