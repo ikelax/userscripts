36822c7a739b84edf8970dbbe4978d8710ce2ae0
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-04-16 19:59:57 +0200

    Add Mensaar userscript
    The script adds to the navbar of the page a link for the meal plan of
    the UdS and the HTW.
    
    See also: https://mensaar.de/

---
+++ mensaar-add-uds-htw.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        Mensaar
+// @namespace   Violentmonkey Scripts
+// @match       https://mensaar.de/
+// @grant       none
+// @version     0.1.0
+// @author      Alexander Ikonomou
+// @description 13/04/2025, 23:29:17
+// ==/UserScript==
+
+const navbar = document.querySelector('[class="mr-auto navbar-nav"]');
+const mensaarUrl = "https://mensaar.de/#/menu/";
+
+addMensa("UdS", "sb");
+addMensa("HTW", "htwcas");
+
+if (navbar != null) {
+  return;
+}
+
+function addMensa(title, location) {
+  const mensa = document.createElement("a");
+
+  var titleNode = document.createTextNode(title);
+  mensa.appendChild(titleNode);
+
+  // Just for consistency
+  mensa.href = mensaarUrl + location;
+  mensa.classList.add("nav-link", "active");
+  mensa.addEventListener("click", () => goToMenu(location));
+
+  navbar.appendChild(mensa);
+}
+
+function goToMenu(location) {
+  window.location.href = mensaarUrl + location;
+  window.location.reload();
+}
