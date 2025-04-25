319e012bcc31fd79fc2a40671279f25f70434f83
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-04-25 21:42:47 +0200

    Fix require for waitForKeyElements
    The old version used an external script that was not approved by
    Greasy Fork.

--- userscripts/mensaar-show-next-day-when-closed.user.js
+++ userscripts/mensaar-show-next-day-when-closed.user.js
@@ -6,7 +6,7 @@
 // @version      0.1.0
 // @author       Alexander Ikonomou
 // @description  25/04/2025, 15:32:45
-// @require      https://raw.githubusercontent.com/CoeJoder/waitForKeyElements.js/refs/heads/master/waitForKeyElements.js
+// @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
 // ==/UserScript==
 
 // eslint-disable-next-line no-undef
