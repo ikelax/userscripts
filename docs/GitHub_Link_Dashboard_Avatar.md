2acce058e8e900d31fb0e428ac29a56e2073e71a
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-05-27 17:51:08 +0200

    Link avatar on GitHub dashboard to profile or org
    The userscripts links the avatar on the dashboard to its profile or
    organization.
    
    The GreasyFork url will be added after I have released the userscript
    there.
    
    Closes: https://github.com/ikelax/userscripts/issues/38

---
+++ docs/GitHub_Link_Dashboard_Avatar.md
@@ -0,0 +1,8 @@
+# GitHub Link Dashboard Avatar
+
+A userscript that links the avatar on the dashboard to its
+profile or organization.
+
+## Screenshot
+
+![Location of the avatar on the dashboard](../images/glda.png)
---
+++ userscripts/github-link-dashboard-avatar.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name             GitHub Link Dashboard Avatar
+// @namespace        https://github.com/ikelax/userscripts
+// @match            https://github.com/
+// @match            https://github.com/orgs/*/dashboard
+// @grant            none
+// @version          0.1.0
+// @author           Alexander Ikonomou
+// @description      A userscript that links the avatar on the dashboard to its profile or organization.
+// @license          MIT
+// @supportURL       https://github.com/ikelax/userscripts/issues
+// @updateURL        https://raw.githubusercontent.com/ikelax/userscripts/refs/heads/master/userscripts/github-link-dashboard-avatar.user.js
+// @downloadURL      https://raw.githubusercontent.com/ikelax/userscripts/refs/heads/master/userscripts/github-link-dashboard-avatar.user.js
+// @copyright        2025, Alexander Ikonomou (https://github.com/ikelax)
+// @homepageURL      https://github.com/ikelax/userscripts
+// @homepage         https://github.com/ikelax/userscripts
+// @contributionURL  https://github.com/ikelax/userscripts
+// @collaborator     ikelax
+// @icon             https://github.githubassets.com/pinned-octocat.svg
+// ==/UserScript==
+
+(() => {
+  "use strict";
+
+  const avatar = document.querySelector(
+    "div.dashboard-sidebar img.avatar-small",
+  );
+
+  if (avatar == null) {
+    return;
+  }
+
+  const link = document.createElement("a");
+  link.setAttribute("href", getLink(avatar.getAttribute("alt")));
+
+  wrap(avatar, link);
+
+  /**
+   * Wraps `elementToWrap` with `outerElement`.
+   *
+   * @example `elementToWrap` is an image `<img />`.
+   * `outerElement` is a link `<a href="https://"><a>
+   *
+   * @param {*} elementToWrap The inner element
+   * @param {*} outerElement The outer element
+   */
+  function wrap(elementToWrap, outerElement) {
+    outerElement.innerHTML = elementToWrap.outerHTML;
+    elementToWrap.parentNode.insertBefore(outerElement, elementToWrap);
+    elementToWrap.remove();
+  }
+
+  /**
+   * @param {*} atName The reference to a user or an organization
+   * @returns The link to the profile of the user or the organization
+   */
+  function getLink(atName) {
+    const name = atName.replace("@", "");
+    return `https://github.com/${name}`;
+  }
+})();
